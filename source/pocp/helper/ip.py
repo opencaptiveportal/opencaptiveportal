3f24406ef65e07f654815db6b8eb05e6c5401462
tim.at.switch <tim.at.switch@8d695a54-fc33-11dd-a7be-4b3ef3b0700c>
2009-10-22 13:05:47 +0000

    GRE tunnels

--- source/pocp/helper/ip.py
+++ source/pocp/helper/ip.py
@@ -2,7 +2,7 @@
 #  -*- coding: UTF-8 -*-
 # vim: set fileencoding=UTF-8 :
 
-# Helper functions for GRE tunnels
+# Helper functions for GRE tunnels and ip rules
 
 
 class iptExc(Exception):
@@ -20,10 +20,13 @@ def delete_tunnel(gre_tunnel):
   Delete a GRE tunnel.
   """
   import os
+  # routing tables are delete with the GRE tunnels
+  # rules have to be deleted separately
   try:
     os.system("""
-        /usr/bin/sudo /bin/ip tunnel del gre%i
-      """ % (gre_tunnel, ))
+        /usr/bin/sudo /bin/ip tunnel del gre%i >/dev/null 2>&1
+        /usr/bin/sudo /bin/ip rule del fwmark %s lookup %s >/dev/null 2>&1
+      """ % (gre_tunnel, str(hex(gre_tunnel)), str(gre_tunnel)))
   except:
     raise iptExc("Could not delete GRE tunnel gre%i" % (gre_tunnel, ))
   return True
@@ -38,6 +41,14 @@ def create_tunnel(gre_tunnel, local_ipv4, remote_ipv4, int_ipv4):
     raise iptExc("Could not create GRE tunnel gre%i with local IPv4 %s and remote IPv4 %s" % (gre_tunnel, local_ipv4, remote_ipv4))
   def raise_ip():
     raise iptExc("Could not add IP addresse %s to GRE tunnel gre%i" % (int_ipv4, gre_tunnel))
+  # Setting ip rule
+  try:
+    ret = os.system("""
+        /usr/bin/sudo /bin/ip rule add fwmark %s lookup %s
+      """ % (str(hex(gre_tunnel)), str(gre_tunnel)) )
+  except:
+    raise iptExc("Could not set ip ru for gre tunnel %s" % str(gre_tunnel) )
+  # Setting GRE tunnel
   try:
     ret = os.system("""
         /usr/bin/sudo /bin/ip tunnel add gre%i mode gre remote %s local %s;
@@ -47,10 +58,12 @@ def create_tunnel(gre_tunnel, local_ipv4, remote_ipv4, int_ipv4):
       raise_gre()
   except:
     raise_gre()
+  # Setting IP configuration
   try:
     ret = os.system("""
-        /usr/bin/sudo /bin/ip addr add %s/30 dev gre%i
-      """ % (int_ipv4, gre_tunnel))
+        /usr/bin/sudo /bin/ip addr  add %s/30 dev gre%i
+        /usr/bin/sudo /bin/ip route add default dev gre%i table %i
+      """ % (int_ipv4, gre_tunnel, gre_tunnel, gre_tunnel))
     if ret != 0:
       raise_ip()
   except:
@@ -58,16 +71,33 @@ def create_tunnel(gre_tunnel, local_ipv4, remote_ipv4, int_ipv4):
   return True
 
 
-def make_gre_tunnel():
+def create_all_gre():
   """
-  Renew all GRE Tunnel and IP addresses. 
+  Create all GRE tunnels and IP configurations
   """
   from pocp.ocp.models import provider
   import os
   ret = True
   for p in provider.objects.all():
-    delete_tunnel(p.gre_tunnel)
+    try:
+      delete_tunnel(p.gre_tunnel)
+    except:
+      pass
     create_tunnel(p.gre_tunnel, p.local_ipv4, p.remote_ipv4, p.int_ipv4)
   return True
 
+
+def delete_all_gre():
+  """
+  Delete all GRE tunnels and IP configurations
+  """
+  from pocp.ocp.models import provider
+  import os
+  ret = True
+  for p in provider.objects.all():
+    try:
+      delete_tunnel(p.gre_tunnel)
+    except:
+      pass
+  return True
 
--- source/pocp/shell/iptables-restore.py
+++ source/pocp/shell/iptables-restore.py
@@ -6,7 +6,12 @@
 
 import sys
 sys.path.append("../../")
-from pocp.helper.iptables import make_iptables
 
+# Reload iptables
+from pocp.helper.iptables import make_iptables
 p = make_iptables()
+
+# Reload IP configuration and GRE tunnels
+from pocp.helper.ip import create_all_gre
+p = create_all_gre()
 
